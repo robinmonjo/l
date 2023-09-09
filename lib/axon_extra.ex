b3c5a1fcdc135629177868ba272a779108ddfaba
robinmonjo <robinmonjo@gmail.com>
2023-09-09 15:29:35 +0200

    use op_name for custom layer

--- lib/axon_extra.ex
+++ lib/axon_extra.ex
@@ -2,11 +2,11 @@ defmodule AxonExtra do
   def nodes_id_name_mapping(%Axon{} = model) do
     {mapping, _} =
       Axon.reduce_nodes(model, {%{}, %{}}, fn node, {mapping, counts} ->
-        count = Map.get(counts, node.op, 0)
+        count = Map.get(counts, node.op_name, 0)
 
         {
-          Map.put(mapping, node.id, "#{node.op}_#{count}"),
-          Map.put(counts, node.op, count + 1)
+          Map.put(mapping, node.id, "#{node.op_name}_#{count}"),
+          Map.put(counts, node.op_name, count + 1)
         }
       end)
 

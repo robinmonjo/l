c83b98a26cef0ba1a311feaf4c3c9d054dfb141a
robinmonjo <robinmonjo@gmail.com>
2023-09-10 12:34:49 +0200

    general relu

---
+++ lib/axon_extra/activations.ex
@@ -0,0 +1,19 @@
+defmodule AxonExtra.Activations do
+  import Nx.Defn
+
+  defn relu(t, opts \\ []) do
+    opts = keyword!(opts, [leak: 0.0, sub: 0.0])
+
+    leak = opts[:leak]
+    sub = opts[:sub]
+
+    relu =
+      if leak == 0 do
+        Axon.Activations.relu(t)
+      else
+        Axon.Activations.leaky_relu(t, alpha: leak)
+      end
+
+    if sub == 0, do: relu, else: Nx.subtract(relu, sub)
+  end
+end

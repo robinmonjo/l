e56c19b9e746990d5356f7489b1020fb861d1a51
robinmonjo <robinmonjo@gmail.com>
2023-11-06 21:55:09 +0100

    mix fmt

--- lib/axon_extra/schedulers.ex
+++ lib/axon_extra/schedulers.ex
@@ -8,7 +8,7 @@ defmodule AxonExtra.Schedulers do
   defnp apply_one_cycle_lr(step, opts \\ []) do
     opts =
       keyword!(opts,
-        initial_lr: 1.0e-2,
+        initial_lr: 1.0e-4,
         max_lr: 0.6,
         steps: 1000,
         pct_start: 0.3,
@@ -22,14 +22,13 @@ defmodule AxonExtra.Schedulers do
         # "warmup" phase
         theta = Nx.Constants.pi() * step / break
         {1 - Nx.cos(theta), opts[:initial_lr]}
-
       else
         theta = Nx.Constants.pi() * (step - break) / (opts[:steps] - break)
         {1 + Nx.cos(theta), opts[:min_lr]}
       end
 
-    cos # in the 0..2 range
-    |> Nx.divide(2) # back to
+    cos
+    |> Nx.divide(2)
     |> Nx.multiply(opts[:max_lr] - alpha)
     |> Nx.add(alpha)
   end

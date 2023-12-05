9dcbf92ff33c07dede8b95aa8eb550a21e139bd9
robinmonjo <robinmonjo@gmail.com>
2023-12-05 21:38:27 +0100

    hardware helper

---
+++ lib/l/hw.ex
@@ -0,0 +1,23 @@
+defmodule L.HW do
+  def inspect do
+    EXLA.Client.get_supported_platforms()
+    |> Map.keys()
+    |> Enum.filter(&(&1 != :interpreter))
+    |> Enum.each(fn name ->
+      %EXLA.Client{device_count: count} = EXLA.Client.fetch!(name)
+      IO.puts("#{name}, #{count}")
+    end)
+  end
+
+  def set(client, device_id \\ 0)
+  def set(:cpu, device_id), do: set(:host, device_id)
+  def set(:gpu, device_id), do: set(:cuda, device_id)
+
+  def set(client, device_id) do
+    set_nx_backend(client: client, device_id: device_id)
+  end
+
+  defp set_nx_backend(opts) do
+    Nx.default_backend({EXLA.Backend, opts})
+  end
+end
